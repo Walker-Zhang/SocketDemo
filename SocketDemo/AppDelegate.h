1eb097c47cbd4d1fdaf023060563198446c3bc20
qianfeng <apple@aofel.local>
2014-04-21 15:40:43 +0800

    Initial Commit

---
+++ SocketDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SocketDemo
+//
+//  Created by qianfeng on 14-4-21.
+//  Copyright (c) 2014年 aofel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
